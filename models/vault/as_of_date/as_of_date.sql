a6bb0ce1ffc9d1007774af488936dff18023e4d8
Miguel P Z <60221874+MiguelElGallo@users.noreply.github.com>
2023-08-11 10:04:19 +0300

    as of date

--- models/vault/as_of_date/as_of_date.sql
+++ models/vault/as_of_date/as_of_date.sql
@@ -1,41 +1,15 @@
 
-{%- set datepart = "day" -%}
-{%- set start_date = "dateadd(year, -5, CURRENT_DATE)" -%}
-{%- set end_date = "dateadd(day, 1, CURRENT_DATE)" -%}
 
-with as_of_date as (
+{{ config(materialized='table') }}
+
+{%- set datepart = "day" -%}
+{%- set start_date = "TO_DATE('2023/07/01', 'yyyy/mm/dd')" -%}
+{%- set end_date = "TO_DATE('2023/09/01', 'yyyy/mm/dd')" -%}
+
+WITH as_of_date AS (
     {{ dbt_utils.date_spine(datepart=datepart, 
                             start_date=start_date,
                             end_date=end_date) }}
 )
-,dates as (
-    select date_{{datepart}} as as_of_date from as_of_date
-)
-, daily as (
-  select as_of_date
-  from dates
-  where as_of_date >= dateadd('DAY', -30, current_date) 
-) 
-, monthly as (
-  select trunc(as_of_date, 'MONTH') as_of_date
-  from dates
-  where as_of_date between dateadd('YEAR', -2, current_date) and dateadd('DAY', -30, current_date)
-) 
-, yearly as (
-  select trunc(as_of_date, 'YEAR') as_of_date
-  from dates
-  where as_of_date between dateadd('YEAR', -5, current_date) and dateadd('YEAR', -2, current_date)
-) 
-, combined_dates AS (
-  select as_of_date
-  from yearly
-  union
-  select as_of_date
-  from monthly
-  union
-  select as_of_date
-  from daily
-)
-select distinct dateadd(ms, -1, dateadd(day, 1, to_timestamp(as_of_date))) as_of_date
-from combined_dates
-order by 1 asc
+
+SELECT DATE_{{datepart}} as AS_OF_DATE FROM as_of_date
